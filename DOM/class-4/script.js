8d144c55bccb5595bca8e5f5397d36211588ffd8
akashm01github <akashmukhopadhayay@gmail.com>
2026-07-06 12:55:47 +0530

    Event Deligation

---
+++ DOM/class-4/script.js
@@ -0,0 +1,12 @@
+const btnFive = document.querySelector("#five");
+
+const main = document.querySelector("main");
+
+// btnFive.addEventListener("click",(event)=>{
+//     console.log(event.target)
+// })
+
+
+// main.addEventListener("click",(event)=>{
+//     console.log(event.target);
+// })
