26ce80efb2ccaf29ce7b8a31300edaa1fd0e95c4
akashm01github <akashmukhopadhayay@gmail.com>
2026-07-07 21:11:20 +0530

    Project-2

---
+++ DOM/class-5/script.js
@@ -0,0 +1,73 @@
+const main = document.querySelector("main");
+// const box = document.querySelector(".box");
+const btn = document.querySelector("button");
+const timer = document.querySelector("#timer");
+const scoreBoard = document.querySelector("#scoreBoard");
+const box = document.createElement("div");
+
+const overlay = document.querySelector(".overlay");
+
+box.classList.add("box");
+
+
+let time = 0;
+let interval;
+let score = 0;
+let timeout;
+
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    return `rgb(${r},${g},${b})`;
+}
+
+const randomBox = () => {
+
+
+    main.append(box);
+
+    let mainH = main.clientHeight - box.offsetHeight;
+
+    let mainW = main.clientWidth - box.offsetWidth;
+
+    const rx = Math.floor(Math.random() * mainW);
+    const ry = Math.floor(Math.random() * mainH);
+
+    box.style.top = `${ry}px`
+    box.style.left = `${rx}px`
+
+    box.style.backgroundColor = randomColor();
+}
+
+
+btn.addEventListener("click", () => {
+    clearInterval(interval);
+
+
+    randomBox();
+
+
+    interval = setInterval(() => {
+
+        randomBox();
+
+        time += 1;
+        timer.textContent = time;
+
+
+    }, 1000);
+
+
+    timeout = setTimeout(() => {
+        clearInterval(interval)
+        overlay.style.display = "flex";
+    }, 10000);
+})
+
+
+box.addEventListener("click", () => {
+    score += 1;
+    scoreBoard.textContent = score;
+})
